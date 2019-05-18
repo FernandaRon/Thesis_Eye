bf935de9a3abba765c19e95ec3a3ed23991f028b
FernandaRon <rondon.fernanda11@gmail.com>
2019-05-18 17:03:02 -0500

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 // RHL only for front end development
 import { hot } from 'react-hot-loader';
+import Header from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/Header.jsx';
+import '/home/hc-29/Documents/Fer/thesis_eye/client/src/dist/Header.css'
 
 class App extends Component {
   render() {
-    return <div>Welcome to a Modern Minimal React Boilerplate</div>;
+    return<div>
+      <Header /> 
+      <div>Welcome to a Modern Minimal React Boilerplate</div>;
+    </div>
   }
 }
 // hot export works with RHL. Remove line 11 when starting fullstack integration
-export default hot(module)(App);
+//export default hot(module)(App);
 // Uncomment line 13 & delete line 11 when starting fullstack integration
-// export default App;
+ export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const Header = (props) => (
+
+  <div className="header">
+  <h1>Header</h1>
+</div>
+
+  )
+
+
+
+export default Header
+
