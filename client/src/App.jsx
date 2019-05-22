6591936f6a95e686644da8bf9c5270cdb884e23a
FernandaRon <rondon.fernanda11@gmail.com>
2019-05-22 18:53:55 -0500

    Header with p5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 // RHL only for front end development
 import { hot } from 'react-hot-loader';
-import Header from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/Header.jsx';
+import Header from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/Header.js';
 import '/home/hc-29/Documents/Fer/thesis_eye/client/src/dist/Header.css'
+import P5Wrapper from 'react-p5-wrapper';
 
 class App extends Component {
   render() {
     return<div>
-      <Header /> 
+      <Header />
       <div>Welcome to a Modern Minimal React Boilerplate</div>;
     </div>
   }
