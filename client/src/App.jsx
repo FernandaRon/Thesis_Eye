4c7ae18d2126e4fb6a183e5aaa3c032012f9e53a
FernandaRon <rondon.fernanda11@gmail.com>
2019-05-23 19:44:26 -0500

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import { hot } from 'react-hot-loader';
 import Header from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/Header.js';
 import '/home/hc-29/Documents/Fer/thesis_eye/client/src/dist/Header.css'
 import P5Wrapper from 'react-p5-wrapper';
+import NavbarPage from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/NavBar.jsx'
 
 class App extends Component {
   render() {
     return<div>
+      <NavbarPage />
       <Header />
       <div>Welcome to a Modern Minimal React Boilerplate</div>;
     </div>
