5bfa65239c066e584fd0cf6d0105de905493864e
FernandaRon <rondon.fernanda11@gmail.com>
2019-05-25 12:43:29 -0500

    fixing bug with fontaw && react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,16 @@ import Contact from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/
 import About from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/About.jsx';
 import Home from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/Home.jsx';
 import Links from '/home/hc-29/Documents/Fer/thesis_eye/client/src/components/Link.jsx';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter} from '@fortawesome/free-brands-svg-icons';
+
+library.add(faTwitter)
+
+
+
+
+
 class App extends Component {
   render() {
     return<div>
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import ref from '/home/hc-29/Documents/Fer/thesis_eye/client/src/dist/RRSS.css'
+//import ref from '/home/hc-29/Documents/Fer/thesis_eye/client/src/dist/RRSS.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
 
 
 const Contact = (props) => (
+
+
   <div>
 
-  <a href="https://twitter.com/cotuh_" className="fa fa-twitter"></a>
-  <a href="https://www.linkedin.com/in/fernanda-rondon-450086186" className="fa fa-linkedin"></a>
-  <a href="https://www.instagram.com/fernandarondon_" className="fa fa-instagram"></a>
+  <a href="https://twitter.com/cotuh_"><img src={<FontAwesomeIcon icon={['fal', 'twitter']}/>}></img></a>
+//  <a href="https://www.linkedin.com/in/fernanda-rondon-450086186" className="fa fa-linkedin"></a>
+//  <a href="https://www.instagram.com/fernandarondon_" className="fa fa-instagram"></a>
   </div>
 
   )
