94822d39188c98c07285b83e2d81b33657e3bc03
FernandaRon <rondon.fernanda@gmail.com>
2019-05-31 12:16:02 -0500

    Header home

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,8 @@ import React from 'react';
 const Header = (props) => (
   <div id="header">
     <header>
-
+      <img src ="https://i.imgur.com/YqG9P5fh.png"  width="100%"  height="auto">
+      </img>
       </header>
   </div>
 
