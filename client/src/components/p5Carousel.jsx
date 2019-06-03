5647e9f47d7a863216977e469a6fadb5cf990760
FernandaRon <rondon.fernanda@gmail.com>
2019-06-03 12:37:55 -0500

    codepen embed

---
+++ client/src/components/p5Carousel.jsx
@@ -0,0 +1,28 @@
+ import React from 'react';
+
+
+ const Demo = () => (
+  <div className="row">
+    <div className="row"  margin="0"  padding="20" right="150px">
+      <iframe height="400" width="100%" scrolling="no" title="Reaction Algorythm.p5" src="//codepen.io/fernandaron/embed/preview/WBPBpq/?height=265&theme-id=light&default-tab=result" frameborder="no" allowtransparency="true" allowfullscreen="true">
+        See the Pen <a href='https://codepen.io/fernandaron/pen/WBPBpq/'>Reaction Algorythm.p5</a> by FernandaRon
+        (<a href='https://codepen.io/fernandaron'>@fernandaron</a>) on <a href='https://codepen.io'>CodePen</a>.
+      </iframe>
+    </div>
+    <div className="row"  margin="0"  padding="20" right="150px">
+      <iframe height="265" width="100%;" scrolling="no" title="CellsMaker" src="//codepen.io/fernandaron/embed/preview/QRYeJz/?height=265&theme-id=light&default-tab=result" frameborder="no" allowtransparency="true" allowfullscreen="true">
+      See the Pen <a href='https://codepen.io/fernandaron/pen/QRYeJz/'>CellsMaker</a> by FernandaRon
+      (<a href='https://codepen.io/fernandaron'>@fernandaron</a>) on <a href='https://codepen.io'>CodePen</a>.
+      </iframe>
+    </div>
+    <div>
+    <iframe height="369" width="100%" scrolling="no" title="oRVvWo" src="//codepen.io/fernandaron/embed/preview/oRVvWo/?height=369&theme-id=light&default-tab=result" frameborder="no" allowtransparency="true" allowfullscreen="true">
+    See the Pen <a href='https://codepen.io/fernandaron/pen/oRVvWo/'>oRVvWo</a> by FernandaRon
+    (<a href='https://codepen.io/fernandaron'>@fernandaron</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>
+    </div>
+  </div>
+
+ )
+
+ export  default Demo;
