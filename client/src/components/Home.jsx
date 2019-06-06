9e2db063576ea3734420b2c4ad73d273ff4d2a62
FernandaRon <rondon.fernanda@gmail.com>
2019-06-06 10:08:12 -0500

    banner aboutMe

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React  from 'react';
 import Header from  './Header.js'
+import AboutMe from './aboutMe.jsx'
 
  const Home= () => (
+  <div>
    <Header />
-
+    <AboutMe />
+    </div>
 
  )
 
---
+++ client/src/components/aboutMe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../dist/Footer.css'
+
+
+const AboutMe = (props) => (
+
+
+<img className="me" src="https://i.imgur.com/COuPSKIm.png" width="300px" height="300px"/>
+
+
+)
+ export default AboutMe;
