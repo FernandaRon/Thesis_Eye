b40b83b606925d958dc5bd7baf33b0cc6222ac62
FernandaRon <rondon.fernanda@gmail.com>
2019-06-10 11:01:22 -0500

    Proyects on landing page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,12 +2,14 @@ import React  from 'react';
 import Header from  './Header.js'
 import AboutMe from './aboutMe.jsx'
 import Description from './Description.jsx'
+import Work from './Work.jsx'
 
  const Home= () => (
   <div>
    <Header />
     <AboutMe />
       <Description />
+      <Work />
     </div>
 
  )
---
+++ client/src/components/Work.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../dist/description.css'
+
+const Work = (props) => (
+  <div className="works">
+    <p><strong>Whacky Waving Inflatable Arm Flailing Tube Man</strong> 2019<br/>
+          <div className="des">
+          See how many tube man can dance at the same time.<br/>
+          JavaScript, jQuery, and CSS.
+          </div>
+    </p>
+    <p><strong>VENEXICO</strong> 2019<br/>
+      <div className="des">
+      Find out the meaning between Venezuelan and Mexican slangs<br/>
+      ReactJS, MySQL, Node, Express.
+      </div>
+    </p>
+    <p><strong>InkPlace</strong> 2019 <br/>
+      <div className="des">
+      A place where you can book your appointments with a Tattoo studio of your style preference<br/>
+      ReactJS, ReactRouter, MySQL, node, Express, CSS3.
+      </div>
+    </p>
+ </div>
+
+  )
+
+
+
+export default Work
