006e80b950cf85cc538f406a8e88ae42e59c626d
FernandaRon <rondon.fernanda@gmail.com>
2019-06-10 10:11:51 -0500

    banner text

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -5,11 +5,13 @@ import '../dist/description.css'
 
 
 
-const Description = (props) => (<div>
+const Description = (props) => (<div  className="whole">
   <span className="hone">Creative</span><br />
  <span className="hone1">Logic</span><br/>
+ <div className="test">
     <span className="hone2">Coder</span><br />
       <span className="hone3">Artist</span><br />
+      </div>
           <span className="hone4"> Made in Venezuela </span>
 
 </div>
